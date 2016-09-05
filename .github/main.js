9d4028f8c4a73f5dc42fc41bd4a9cea7b26fc4fb
Philippe Charrière <k33g@github.com>
2016-09-05 08:47:06 +0200

    Create main.js

---
+++ .github/main.js
@@ -0,0 +1,9 @@
+console.log("yo..!");
+console.log($("h1"));
+console.log("Hello World ...");
+console.log($("h2"));
+
+$.get("https://api.github.com/users/k33g").then((data) => {
+  console.log(data);
+  $("h5").html(data.avatar_url)
+});
