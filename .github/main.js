3fe6197dae336243228abd3c1f28569fa9e3ae0b
Philippe Charrière <k33g@github.com>
2016-09-08 08:28:04 +0200

    Update main.js

--- .github/main.js
+++ .github/main.js
@@ -6,7 +6,6 @@ console.log($("h2"));
 $.get("https://api.github.com/users/k33g").then((data) => {
   console.log(data);
   $("h5").html(data.avatar_url)
-  $('h5').append("<p>Hello World</p>")
 });
 
 
