9dcbc482cefedb9dd09ad8dfa54e03ecc29c471f
Philippe Charrière <k33g@github.com>
2016-09-08 08:24:11 +0200

    Update main.js

--- .github/main.js
+++ .github/main.js
@@ -7,3 +7,10 @@ $.get("https://api.github.com/users/k33g").then((data) => {
   console.log(data);
   $("h5").html(data.avatar_url)
 });
+
+let content=`<a href="/k33g/cuckoo/help" class="js-selected-navigation-item reponav-item" data-hotkey="g w" data-selected-links="repo_wiki /k33g/cuckoo/wiki">
+      <svg aria-hidden="true" class="octicon octicon-octoface" height="16" version="1.1" viewBox="0 0 16 16" width="16"></svg>
+      🐼 Help
+</a>`;
+
+$('nav').first().append(content)
